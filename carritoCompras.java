9da9efff712f01e996a1de5b7b5547ad517e628d
edgardo2556 <jugador.eo@gmail.com>
2025-02-07 19:08:29 +0100

    carrito

--- carritoCompras.java
+++ carritoCompras.java
@@ -1,15 +1,48 @@
 public class carritoCompras {
-    public static String[] productos = new String[10];
+    public static String[] productosCarro= new String[10];
     public static double [] cantidades = new double[10];
     public static int contador = 0 ;
     public static void agregarCarro(String producto, double cantidad){
-        if (contador < productos.length) {
-            productos[contador] = producto;
+        if (contador < productosCarro.length) {
+            productosCarro[contador] = producto;
             cantidades[contador] = cantidad;
             contador++;
         }
     }
-    public static void  borrarCarrito (String producto , double cantidad){
-
+    public static void  borrarCarrito(String producto){
+        for (int i = 0; i < contador; i++){
+            if (productosCarro[i].equalsIgnoreCase(producto)) {
+                for (int j = 0; j< contador-1; j++) {
+                         productosCarro[j] = productosCarro [j + 1];
+                         cantidades [j] = cantidades[j + 1];
+                }
+                productosCarro[contador - 1] = null  ;
+                cantidades[contador - 1]= 0 ;
+                contador--;
+                break ;
+            }
+        }
     }
+    public static void finalizarCompra() {
+        double total = 0;
+        System.out.println("Productos en el carrito:");
+        for (int i = 0; i < contador; i++) {
+            double precioUnitario = obtenerPrecio(productosCarro[i]);
+            double subtotal = precioUnitario * cantidades[i];
+            total += subtotal;
+            System.out.println(productosCarro[i] + " x " + cantidades[i] + " = $" + subtotal);
+        }
+        System.out.println("Total a pagar: $" + total);
+    }
+
+    private static double obtenerPrecio(String producto) {
+        for (int i = 0; i < main.hierba.length; i++) {
+            if (main.hierba[i].equalsIgnoreCase(producto)) {
+                return main.precio[i];
+            }
+        }
+        return 0;
+    }
+
+
 }
--- ordenar.java
+++ ordenar.java
@@ -1,22 +1,19 @@
 public class ordenar {
     public static void ordenar(boolean ascendente) {
-        int n = main.precio.length;  // Aquí n es 4 si hay 4 elementos
-        for (int i = 0; i < n; i++) {  // El ciclo recorrerá de i = 0 hasta i = n - 1
-            for (int j = 0; j < n - i - 1; j++) {  // Este es el ciclo de burbuja que hace las comparaciones
+        int n = main.precio.length;  // n es 4 si hay 4 elementos
+        for (int i = 0; i < n; i++) {  // recorrerá de i = 0 hasta i = n
+            for (int j = 0; j < n - i - 1; j++) {  // bucle para hacer comparaciones
                 if ((ascendente && main.precio[j] > main.precio[j + 1]) ||
                         (!ascendente && main.precio[j] < main.precio[j + 1])) {
 
-                    // Intercambio de precios
                     double tempPrecio = main.precio[j];
                     main.precio[j] = main.precio[j + 1];
                     main.precio[j + 1] = tempPrecio;
 
-                    // Intercambio de hierba
                     String tempHierba = main.hierba[j];
                     main.hierba[j] = main.hierba[j + 1];
                     main.hierba[j + 1] = tempHierba;
 
-                    // Intercambio de origen
                     String tempOrigen = main.origen[j];
                     main.origen[j] = main.origen[j + 1];
                     main.origen[j + 1] = tempOrigen;
