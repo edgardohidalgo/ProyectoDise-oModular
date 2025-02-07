92a0871a9fbed72d78cb06f9a4d610ce049af627
edgardo2556 <jugador.eo@gmail.com>
2025-02-07 19:46:28 +0100

    cambi

--- buscar.java
+++ buscar.java
@@ -1,8 +1,7 @@
 public class buscar {
     public static void buscarNombre(String nombre) {
         System.out.println("Resultados de búsqueda por nombre");
-        nombre = nombre.toLowerCase();
-
+         nombre = nombre.toLowerCase();
         for (int i = 0; i < main.hierba.length; i++) {
             if (main.hierba[i].toLowerCase().contains(nombre)) {
                 System.out.println("El precio es " + main.precio[i] + "   El origen es " + main.origen[i] + "   La hierba es " + main.hierba[i]);
